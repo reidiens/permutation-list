36ac077f7ddd35d4bf0471f5dfe6763768f5b607
ruby :3 <124638816+reidiens@users.noreply.github.com>
2023-08-04 22:10:00 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 #define CYCLE_ARR for (int i = 0; i < a->arrSize; i++)
 
 typedef struct Array {
-	int *nArr;
+	int *arr;
 	int arrSize;
 } arr_t;
 
 
-void gint(int *num);
-int init_arr(arr_t *a);
-void parr(arr_t *a);
-void swap(int *e1, int *e2);
-void permute(arr_t *a, int arrs);
-long long factorial(int *arrSize);
-int scan_argv(char **argV, int *arrSize);
+void gint(int *num);   // get integer
+int init_arr(arr_t *a); 
+void print_arr(arr_t *a);
+void swap(int *e1, int *e2); 
+void permute(arr_t *a, int arrSize);
+long long factorial(int *arrSize);  // calculate factorial
+int scan_argv(char **argV, int *arrSize);  // check if an integer is the argument
 
 int main(int argC, char *argV[]) {
 	arr_t a;
@@ -34,20 +34,20 @@ int main(int argC, char *argV[]) {
 
 	if (!init_arr(&a)) {
 		fprintf(stderr, "array init error\n");
-		free(a.nArr);
+		free(a.arr);
 		return 1;
 	}
 
 	permute(&a, a.arrSize);
 	printf("there are %llu permutations for a %d element array.", factorial(&a.arrSize), a.arrSize);
-	free(a.nArr);
+	free(a.arr);
 }
 
 void gint(int *num) {
 	char *temp = (char *)malloc(128);
 	fgets(temp, 128, stdin);
 	if (strlen(temp) > 1) {
-		if (!sscanf(temp, "%d", num) || temp[0] == ' ') {
+		if (!sscanf(temp, "%d", num) || temp[0] == ' ') {  // for some reason, ' ' makes it through sscanf when looking for %d
 			puts("try again:");
 			free(temp);
 			gint(num);
@@ -65,25 +65,25 @@ void gint(int *num) {
 }
 
 int init_arr(arr_t *a) {
-	a->nArr = malloc(a->arrSize * sizeof(*a->nArr));
+	a->arr = malloc(a->arrSize * sizeof(*a->arr));
 
 	CYCLE_ARR {
-        a->nArr[i] = i + 1;
+        a->arr[i] = i + 1;  // init to 1, 2, 3, etc
     }
 
 	CYCLE_ARR {
-		if ((a->nArr[i] == '\0') && i <a->arrSize) {
-			return 0;
+		if ((a->arr[i] == '\0') && (i < a->arrSize)) {
+			return 0; // 0 = error for formatting purposes (see line 35)
 		}
 	}
 	return 1;
 }
 
-void parr(arr_t *a) {
+void print_arr(arr_t *a) {
 	CYCLE_ARR {
-		printf("%d ", a->nArr[i]);
+		printf("%d ", a->arr[i]);
 	}
-	puts(" ");
+	puts(" "); // insert a newline after.
 }
 
 void swap (int *e1, int *e2) {
@@ -92,19 +92,19 @@ void swap (int *e1, int *e2) {
 	*e2 = temp;
 }
 
-void permute(arr_t *a, int arrs) {
-	if (arrs == 1) {
-		parr(a);	
+void permute(arr_t *a, int arrSize) {
+	if (arrSize == 1) {
+		print_arr(a);	
 		return;	
 	}
-	for (int i = 0; i < arrs; i++) {
-		permute(a, arrs - 1);
+	for (int i = 0; i < arrSize; i++) {
+		permute(a, arrSize - 1);
 		
-		if (arrs % 2 == 1) {
-			swap(&a->nArr[0], &a->nArr[arrs - 1]);
+		if (arrSize % 2 == 1) {
+			swap(&a->arr[0], &a->arr[arrSize - 1]);
 		}	
 		else {
-			swap(&a->nArr[i], &a->nArr[arrs - 1]);
+			swap(&a->arr[i], &a->arr[arrSize - 1]);
 		}
 	}
 }
